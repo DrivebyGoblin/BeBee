d6f019f67ac5e6c02905cab9fce49fef342fa1c5
DrivebyGoblin <drivebyromanoff@mail.ru>
2023-05-08 11:21:55 +0700

    fix bugs

--- Assets/Scripts/FallingThorn.cs
+++ Assets/Scripts/FallingThorn.cs
@@ -27,7 +27,7 @@ public class FallingThorn : MonoBehaviour
 
     private IEnumerator DestroyFallingObject(Rigidbody2D _rb)
     {
-        yield return new WaitForSeconds(5f);
+        yield return new WaitForSeconds(2f);
         Destroy(_rb.gameObject);
     }
 
--- Assets/Scripts/Obstacles/MovingThroun.cs
+++ Assets/Scripts/Obstacles/MovingThroun.cs
@@ -19,7 +19,6 @@ public class MovingThroun : MonoBehaviour
 
     public void StopThroun()
     {
-        print("salam");
         transform.position = StartPos;
     }
 
